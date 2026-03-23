9e2f6c646cf6d509f59066b48982e9021449d35f
itsme040305 <muskan2005004@gmail.com>
2026-03-23 11:07:17 +0530

    req and procfile added

--- api.py
+++ api.py
@@ -105,5 +105,7 @@ def chat():
 
     return jsonify({"response": response})
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
